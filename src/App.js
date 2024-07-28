a94fd6047169529976bf0abc8d54d55b3fff996a
CoderRocha <guilhermerochas0420@gmail.com>
2024-07-27 22:54:25 -0300

    added comparing card choices functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -30,6 +30,27 @@ function App() {
   // handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+
+      if(choiceOne.src === choiceTwo.src) {
+        console.log('You have a match!')
+        resetTurn()
+      } else {
+        console.log('You dont have a match. Try again!')
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
   return (
